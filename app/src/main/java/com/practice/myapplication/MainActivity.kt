ba8b920fe88e90862c47a32007f1aeedaf73716e
Bot <tanjimahmed327@gmail.com>
2024-11-23 22:57:29 +0600

    1.0.0

---
+++ app/src/main/java/com/practice/myapplication/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.practice.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        //step 2:Declaring Views
+        val edt:EditText=findViewById(R.id.edt)
+        val btn:Button=findViewById(R.id.btn)
+        //step 3:Adding functionalities
+        btn.setOnClickListener {
+            val enteredText = edt.text.toString()
+            Toast.makeText(this@MainActivity, "Hello $enteredText", Toast.LENGTH_LONG).show()
+        }
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
